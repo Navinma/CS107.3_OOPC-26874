6b793e1f1cd1a16e15e97c4f114c52745b8a0d01
Navinma <136572618+Navinma@users.noreply.github.com>
2023-07-26 10:49:44 +0530

    Add files via upload

---
+++ Lab_09/Lab_09/Lab_09/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int min, max, i, avg;
+            int[] Array = new int[10];
+            Console.WriteLine("Enter elements in the array:");
+            for (i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("number" + i);
+            }
+            max = Array[0];
+            min = Array[0];
+            for (i = 1; i <= 10; i++)
+            {
+                if (Array[i] > max)
+                {
+                    Array[i] = max;
+                }
+
+                if (Array[i] < min)
+                {
+                    Array[i] = min;
+                }
+            }
+        }
+    }
+}
---
+++ Lab_09/Lab_9.3/Lab_9.3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_9._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[10];
+            int sum = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter value:");
+                num[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("num[i]");
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                sum = sum + num[i];
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
